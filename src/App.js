e05eafbd1bc166b75a5ed1333ba333acc5f85d5c
Martin Myhre <70258183+martinlime@users.noreply.github.com>
2021-08-19 20:49:59 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import './App.css';
+import React, { useState } from 'react';
+import hug from './img/hug.mp4';
 
 function App() {
+
+  const [goodDay, setgoodDay] = useState("");
+  const [img, setImg] = useState(false);
+
+  function goodDayy() {
+    setImg(false);
+    setgoodDay("That's Awesome!!!")
+  }
+
+  function badDay() {
+    setgoodDay("Oh no, here have a hug")
+    setImg(true);
+  }
+
   return (
     <div>
-      <h1>Dark web is cooool</h1>
-      <h4>Are you having a good day?</h4>
-      <button>Yes</button>
-      <button>No</button>
+      <h2>Are you having a good day?</h2>
+      <button className="yes" onClick={goodDayy}>Yes</button>
+      <button className="no" onClick={badDay}>No</button>
+      <h3>{goodDay}</h3>
+      {img ? 
+        <div>
+          <video src={hug} alt="hug Video"/>
+        </div> 
+        : <> </>}
     </div>
   );
 }
